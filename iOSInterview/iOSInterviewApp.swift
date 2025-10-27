0d1a66689856858583e7e3466ee6c4cd9fa5c4b1
Fadil <fadil@sleip.com>
2025-10-27 09:45:56 +0100

    Code for interview

--- iOSInterview/iOSInterviewApp.swift
+++ iOSInterview/iOSInterviewApp.swift
@@ -12,7 +12,7 @@ import SwiftData
 struct iOSInterviewApp: App {
     var sharedModelContainer: ModelContainer = {
         let schema = Schema([
-            Item.self,
+            Horse.self,
         ])
         let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
 
